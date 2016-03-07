aa93dea18a9134bc8d11631beb22bb5ec38d6c53
Kenny <Kenny>
2016-03-06 21:47:56 -0800

    fixed the problem of sign up page (it went to the main page if we went from sign up page back to login page)

--- app/src/main/java/com/lasergiraffe/rideshare/MainActivity.java
+++ app/src/main/java/com/lasergiraffe/rideshare/MainActivity.java
@@ -150,7 +150,6 @@ public class MainActivity extends ListActivity {
 
         // "Log out" Button for users to log out
         logout.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 ParseUser.logOut();
--- app/src/main/java/com/lasergiraffe/rideshare/OpenedPostActivity.java
+++ app/src/main/java/com/lasergiraffe/rideshare/OpenedPostActivity.java
@@ -1,19 +1,13 @@
 package com.lasergiraffe.rideshare;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import com.lasergiraffe.rideshare.util.SystemUiHider;
 import com.parse.FindCallback;
