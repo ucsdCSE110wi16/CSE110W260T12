55ee7d8d72c53e403c92e13c636e61261c4980fc
Amit <a2nijjar@ucsd.edu>
2016-03-08 15:17:11 -0800

    added a test

--- app/src/androidTest/java/com/lasergiraffe/rideshare/MainActivityTest.java
+++ app/src/androidTest/java/com/lasergiraffe/rideshare/MainActivityTest.java
@@ -1,11 +1,16 @@
 package com.lasergiraffe.rideshare;
 
 
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by Amit on 3/5/2016.
@@ -18,6 +23,7 @@ public class MainActivityTest {
     public final ActivityRule<MainActivity> mActivityRule = new ActivityRule<>(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void shouldLauchMainScreen() {
+        onView(withText("Hello")).check(ViewAssertions.matches(isDisplayed()));
     }
 }
