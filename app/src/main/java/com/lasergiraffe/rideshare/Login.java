cd607ef4b8ddc2ac21628ae27ff2577d37f1b366
Kenny <Kenny>
2016-03-12 00:11:51 -0800

    Changed OpenPostActivity

--- app/src/main/java/com/lasergiraffe/rideshare/Login.java
+++ app/src/main/java/com/lasergiraffe/rideshare/Login.java
@@ -83,7 +83,7 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(Login.this, SignUp.class);
                 startActivity(i);
-                finish();
+                //finish();
             }
 
 
